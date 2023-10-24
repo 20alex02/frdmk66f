ed530f856227e71cdd18cd3daf522933cad75322
unknown <xpopovi1@ad.fi.muni.cz>
2023-10-24 11:57:01 +0200

    setup

---
+++ week_05_adc/source/week_05_adc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK66F18.h"
+#include "fsl_debug_console.h"
+
+int main(void) {
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+    /* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+#endif
+
+    PRINTF("Hello World\r\n");
+//    LED_GREEN_TOGGLE();
+    while(1) {
+    	SDK_DelayAtLeastUs(1000 * 1000, SystemCoreClock);
+
+    }
+    return 0 ;
+}
